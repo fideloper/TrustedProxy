438d3f3e2e9dd4e477515285d1a2007a5b29ef48
fideloper <fideloper@gmail.com>
2013-12-11 19:09:53 -0500

    add publishable configuration

---
+++ src/config/proxy.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Proxies
+    |--------------------------------------------------------------------------
+    |
+    | Set an array of trusted proxies, so Laravel knows to grab the client's
+    | IP address via the HTTP_X_FORWARDED_FOR header.
+    |
+    | To trust all proxies, use the value '*':
+    |
+    | 'proxies' => '*'
+    |
+    */
+
+    'proxies' => array(),
+
+);
