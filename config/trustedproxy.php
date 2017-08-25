b425627b25036e2a1880300f82d3cf19e058fe29
Luke Policinski <Luke@LukePOLO.com>
2017-08-25 12:11:35 -0400

    Updated Deprecated Variables

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -63,9 +63,9 @@ return [
      */
     'headers' => [
         (defined('Illuminate\Http\Request::HEADER_FORWARDED') ? Illuminate\Http\Request::HEADER_FORWARDED : 'forwarded') => 'FORWARDED',
-        Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
-        Illuminate\Http\Request::HEADER_CLIENT_HOST  => 'X_FORWARDED_HOST',
-        Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
-        Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
+        Illuminate\Http\Request::HEADER_X_FORWARDED_FOR    => 'X_FORWARDED_FOR',
+        Illuminate\Http\Request::HEADER_X_FORWARDED_HOST  => 'X_FORWARDED_HOST',
+        Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+        Illuminate\Http\Request::HEADER_X_FORWARDED_PORT  => 'X_FORWARDED_PORT',
     ]
 ];
