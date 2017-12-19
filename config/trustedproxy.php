cbdf31cbc137ae7a7d439d53cadd5b0da55f42c7
Chris Fidao <fideloper@gmail.com>
2017-12-19 10:18:56 -0600

    Wording updates to config file for stand-alone users

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -12,17 +12,8 @@ return [
      * within TrustedProxy to trust any proxy
      * that connects directly to your server,
      * a requirement when you cannot know the address
-     * of your proxy (e.g. if using Rackspace balancers).
+     * of your proxy (e.g. if using ELB or similar).
      *
-     * The "**" character is syntactic sugar within
-     * TrustedProxy to trust not just any proxy that
-     * connects directly to your server, but also
-     * proxies that connect to those proxies, and all
-     * the way back until you reach the original source
-     * IP. It will mean that $request->getClientIp()
-     * always gets the originating client IP, no matter
-     * how many proxies that client's request has
-     * subsequently passed through.
      */
     'proxies' => null, // [<ip addresses>,], '*'
 
